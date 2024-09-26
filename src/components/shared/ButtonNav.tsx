edf3d5d3f0ef3498f742887a0e33c55fd1ad85a4
GodV759 <nguyendoanh3322@gmail.com>
2024-09-26 21:38:37 +0700

    update code

--- src/components/shared/ButtonNav.tsx
+++ src/components/shared/ButtonNav.tsx
@@ -1,12 +1,19 @@
+import { motion } from "framer-motion";
 import React from "react";
 
 type Props = {
   content: string | React.ReactNode;
+  onClick: () => void;
 };
 
 const ButtonNav = (props: Props) => {
   return (
-    <div className="flex items-center justify-center">{props.content}</div>
+    <motion.button
+      onClick={props.onClick}
+      className="flex items-center justify-center bg-button p-3 rounded-lg"
+    >
+      {props.content}
+    </motion.button>
   );
 };
 
