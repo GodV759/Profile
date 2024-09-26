74923a4bbb4c7e7e2e20ddaae8a0fab5a23ce5b6
GodV759 <nguyendoanh3322@gmail.com>
2024-09-26 21:32:47 +0700

    update code

---
+++ src/components/shared/ButtonNav.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {
+  content: string | React.ReactNode;
+};
+
+const ButtonNav = (props: Props) => {
+  return <div className="flex items-center justify-center"></div>;
+};
+
+export default ButtonNav;
